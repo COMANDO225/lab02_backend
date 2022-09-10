0264b98ae0ce57d80a3503bdd91554e212d49654
Anderson Almeyda <forastero0225@gmail.com>
2022-09-10 02:26:06 -0500

    change db to heroku

--- src/database/db.js
+++ src/database/db.js
@@ -1,7 +1,14 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("solnube_semana2", "root", "", {
-    host: "localhost",
+// const db = new Sequelize("solnube_semana2", "root", "", {
+//     host: "localhost",
+//     dialect: "mysql",
+//     define:{
+//         freezeTableName: true,
+//     }
+// });
+const db = new Sequelize("jbta7idru2ruo55m", "vtduv5wmn83dy4gi", "keadazn3ftowq5ms", {
+    host: "migae5o25m2psr4q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     dialect: "mysql",
     define:{
         freezeTableName: true,
